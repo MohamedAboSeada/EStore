decb3a3819f491979574d90f2f59ad10c484b8e8
MohamedAboSeada <madoaxel287@gamil.com>
2024-09-11 13:24:56 +0300

    add : nav and footer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
 
 // Project Components
-import Home from "./components/HomePage/Home";
+import Home from './components/MohamedAboSeada/HomePage/Home';
+
+// static components
+import Nav from './components/navbar/Nav';
+import Footer from './components/Footer/Footer';
 
 class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-        </Routes>
-      </Router>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<Nav />
+				{/* routes */}
+				<Routes>
+					<Route path='/' element={<Home />}></Route>
+				</Routes>
+				{/* routes */}
+				<Footer />
+			</Router>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/navbar/MidNav.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class MidNav extends Component {
+	render() {
+		return (
+			<div className='mid__header'>
+				<div className='container'>
+					<Link to='/' className='mid__header-logo' href='#'>
+						EStore
+					</Link>
+					<div className='search__field'>
+						<input
+							className='input__field'
+							type='text'
+							placeholder='Search for anything...'
+						/>
+						<div className='search__icon'>
+							<i className='fas fa-search'></i>
+						</div>
+					</div>
+					<div className='mid__btns'>
+						<button className='mid__btn'>
+							<i className='fas fa-cart-shopping'></i>
+						</button>
+						<button className='mid__btn'>
+							<i className='fas fa-heart'></i>
+						</button>
+						<button className='mid__btn'>
+							<i className='fas fa-user'></i>
+						</button>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default MidNav;
